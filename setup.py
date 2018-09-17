9656c3d22bc8c5f18238c55992bd88ebc559602d
Renan Maidana <renanmaidana@gmail.com>
2018-09-17 00:13:49 -0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='predictivecontrol',
+      version='0.1',
+      description='Python package which implements Predictive Control techniques (e.g., MPC, E-MPC)',
+      url='https://github.com/rgmaidana/predictiveControl',
+      author='Renan Maidana',
+      author_email='renan.maidana@acad.pucrs.br',
+      license='MIT',
+      packages=['predictivecontrol'],
+      instal_requires=[
+          'numpy',
+          'scipy'
+      ],
+      zip_safe=False
+    )

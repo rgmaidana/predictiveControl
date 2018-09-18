7b9619e7ead865fe6ec64b119771cf23f01d2366
Renan Maidana <renanmaidana@gmail.com>
2018-09-18 17:31:05 -0300

    Updated version to v0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='predictivecontrol',
-      version='0.3',
+      version='0.4',
       description='Python package which implements Predictive Control techniques (e.g., MPC, E-MPC)',
       url='https://github.com/rgmaidana/predictiveControl',
       author='Renan Maidana',

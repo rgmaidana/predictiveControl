dea458af55530ee370173e6d38023dc696c5c848
Renan Maidana <renanmaidana@gmail.com>
2018-10-18 14:12:49 -0300

    Updated DC motor example

--- examples/dcmotor_mpc.py
+++ examples/dcmotor_mpc.py
@@ -24,6 +24,7 @@ class DCMotor:
         self.T = T
         self.x = np.zeros((self.A.shape[1],1), dtype=np.float)
         self.u = np.zeros((self.B.shape[1],1), dtype=np.float)
+        self.y = np.zeros((self.B.shape[1],1), dtype=np.float)
         
     def output(self, t, x, u=0):
         dx = self.A.dot(x.reshape(self.x.shape)) + self.B.dot(u.reshape(self.u.shape)) # + self.dist
@@ -61,7 +62,7 @@ if __name__ == '__main__':
     
     # Instantiate MPC with DC motor model
     mpc = MPC(motor.A, motor.B, motor.C, T=motor.T)
-    mpc.set_predict_horizon(15)         # Set prediction horizon
+    mpc.set_predict_horizon(10)         # Set prediction horizon
     mpc.set_control_horizon(4)          # Set control horizon
     mpc.dumin, mpc.dumax = np.array([-0.5]), np.array([1.5])    # Set restrictions to actuator variation and amplitude
     mpc.umin, mpc.umax = np.array([-1]), np.array([6])          
@@ -73,7 +74,9 @@ if __name__ == '__main__':
 
     # Run for some seconds
     timeout = 4
-    x = np.array([[0,0],[0,0]], dtype=np.float)
+    x = np.zeros((mpc.A.shape[0],2))
+    u = np.zeros((mpc.B.shape[1],2))
+    y = np.zeros((mpc.C.shape[0],2))
     while True:
         # Run MPC (will update controlled input u)
         mpc.run()
@@ -88,7 +91,10 @@ if __name__ == '__main__':
         # Number of states kept by MPC are bound by prediction horizon, to avoid memory issues on continuous use
         mpc.x = np.roll(mpc.x, -1)
         mpc.x[:,-1] = solv.y
+        mpc.y[:,-1] = mpc.C.dot(mpc.x[:,-1].reshape(mpc.x[:,-1].shape))
         x = np.c_[x, mpc.x[:,-1]]
+        u = np.c_[u, mpc.u[:,-1]]
+        y = np.c_[y, mpc.y[:,-1]]
 
         # Append time
         mpc.t = np.append(mpc.t, mpc.t[-1]+mpc.T)
@@ -97,21 +103,66 @@ if __name__ == '__main__':
 
     # Print results
     print "\nSimulation finished\n"
-    print "Reference: %.2f rad/s" % mpc.get_reference()
-    print "Final states at time %.2f seconds:" % mpc.t[-1]
-    print "\tAngular velocity: \t%.2f rad/s" % mpc.x[0,-1]
-    print "\tAngular acceleration: \t%.2f rad/s^2" % mpc.x[1,-1]
+
+    print "Setpoints:"
+    for i in range(len(y[:,-1])):
+        print "\tR%d: \t%.2f" % (i+1, mpc.get_reference()[i])
+    
+    print "\nFinal states at time %.2f seconds:" % mpc.t[-1]
+    for i in range(len(x[:,-1])):
+        print "\tx%d: \t%.2f" % (i+1, x[i,-1])
+
+    print "\nOutputs at time %.2f seconds:" % mpc.t[-1]
+    for i in range(len(y[:,-1])):
+        print "\ty%d: \t%.2f" % (i+1, y[i,-1])
+
+    print "\nSteady-state error:"
+    for i in range(len(y[:,-1])):
+        print "\ty%d: \t%.2f" % (i+1, mpc.get_reference()[i]-y[i,-1])
 
     # Plot results
     try:
         import matplotlib.pyplot as plt
+
+        # Plot states
         plt.figure()
-        plt.plot(mpc.t, np.ones(mpc.t.shape)*mpc.get_reference(), 'k--', lw=2.0)
-        plt.plot(mpc.t, x[0,:], 'b-', lw=2.0)
+        for k in range(x.shape[0]):
+            plt.plot(mpc.t, x[k,:], lw=2.0)
         plt.xlabel('Time (s)')
-        plt.ylabel('Angular velocity (rad/s)')
-        plt.legend(['Reference', 'Output'])
+        plt.ylabel('x')
+        legend = []
+        for k in range(0,x.shape[0]):
+            legend.append('x%d' % (k+1))
+        plt.legend(legend)
         plt.grid()
+
+        # Plot inputs
+        plt.figure()
+        for k in range(u.shape[0]):
+            plt.plot(mpc.t, u[k,:], lw=2.0)
+        plt.xlabel('Time (s)')
+        plt.ylabel('x')
+        legend = [0 for _ in range(u.shape[0]*2)]
+        for k in range(u.shape[0]):
+            legend[k] = 'u%d' % (k+1)
+        plt.legend(legend)
+        plt.grid()
+
+        # Plot outputs
+        plt.figure()
+        for k in range(y.shape[0]):
+            ax = plt.plot(mpc.t, np.ones(mpc.t.shape)*mpc.get_reference()[k], '--', lw=2.0)
+            plt.plot(mpc.t, y[k,:], color=ax[0].get_color(), lw=2.0)
+        plt.xlabel('Time (s)')
+        plt.ylabel('x')
+        legend = []
+        for k in range(0,y.shape[0]):
+            legend.append('Reference %d' % (k+1))
+            legend.append('y%d' % (k+1))
+        plt.legend(legend)
+        plt.grid()
+
+        # Show figures
         plt.show()
     except ImportError:
         pass
